b2bd506f79eeb8d09af5907ab9752dc704b3e46a
GillinedUp <myrreah@gmail.com>
2017-07-25 15:31:30 +0300

    fixed unclosed array in interface.jl

--- interface.jl
+++ interface.jl
@@ -1,5 +1,5 @@
 include("source.jl")
 pyplot()
-x,y = calc_bif(0.5, 2000)
-verts = [(0, 0.5), (0, 0)
+x,y = calc_bif(0.5, 3000)
+verts = [(0, 0.5), (0, 0)]
 scatter(x, y, marker = (Shape(verts), 1, RGBA(0,0,0,0)), markercolor = :black, leg = false)
