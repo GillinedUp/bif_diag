163f7945d7ced84bb674399e17fea87019097ab4
GillinedUp <myrreah@gmail.com>
2017-07-25 14:54:00 +0300

    changed markers' shape; removed legend

--- interface.jl
+++ interface.jl
@@ -1,4 +1,5 @@
 include("source.jl")
 pyplot()
-x,y = calc_bif(0.5, 1000)
-scatter(x, y, color = :black)
+x,y = calc_bif(0.5, 2000)
+verts = [(0.01, 0.01), (-0.01, 0.01), (-0.01, -0.01), (0.01, -0.01)]
+scatter(x, y, color = :black, marker = (Shape(verts), 1, RGBA(0,0,0,0.2)), leg = false)
