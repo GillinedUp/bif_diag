80ddec5139ef1ee1637c3eb2b61f3e8f5c07a988
GillinedUp <myrreah@gmail.com>
2017-07-25 14:33:44 +0300

    slightly improved performance

--- source.jl
+++ source.jl
@@ -5,28 +5,29 @@ function almost_equal(a, b, ϵ)
 end
 
 function calc_bif(x_a, precision)
-  λ_range = linspace(0.0, 4.0, precision)
+  λ_arr = collect(linspace(0.0, 4.0, precision))
   iter_const = 1000
   ϵ = 1e-6
-  x_arr = Array{Array{Float64, 1}, 1}(0)
-  y_arr = Array{Array{Float64, 1}, 1}(0)
-  for λ in λ_range
+  x_arr = Array{Array{Float64, 1}, 1}(precision)
+  y_arr = Array{Array{Float64, 1}, 1}(precision)
+  for j = 1:precision
+    λ = λ_arr[j]
     x = x_a
-    push!(x_arr, Array{Float64, 1}(0))
-    push!(y_arr, Array{Float64, 1}(0))
+    x_arr[j] = Array{Float64, 1}(0)
+    y_arr[j] = Array{Float64, 1}(0)
     for i = 1:iter_const
       x = λ*x*(1-x)
     end
     while(true)
-      bool_arr = map(y -> almost_equal(x, y, ϵ), y_arr[end]) # check if we already added that point
+      bool_arr = map(y -> almost_equal(x, y, ϵ), y_arr[j]) # check if we already added that point
       if !in(true, bool_arr) # if not
-        push!(y_arr[end], x) # add it to the array
+        push!(y_arr[j], x) # add it to the array
         x = λ*x*(1-x) # iterate once more
       else
         break # else break from the while loop
       end
     end
-    push!(x_arr[end], λ)
+    push!(x_arr[j], λ)
   end
   return x_arr, y_arr
 end
