a7c76c8b46e0bd93e4eb30fe15167d1bb79b84ba
GillinedUp <myrreah@gmail.com>
2017-07-25 12:46:18 +0300

    finally works

--- source.jl
+++ source.jl
@@ -9,21 +9,24 @@ function calc_bif(x_a)
   iter_const = 1000
   ϵ = 1e-6
   x_arr = Array{Array{Float64, 1}, 1}(0)
+  y_arr = Array{Array{Float64, 1}, 1}(0)
   for λ in λ_range
     x = x_a
     push!(x_arr, Array{Float64, 1}(0))
+    push!(y_arr, Array{Float64, 1}(0))
     for i = 1:iter_const
       x = λ*x*(1-x)
     end
     while(true)
-      bool_arr = map(y -> almost_equal(x, y, ϵ), x_arr[end]) # check if we already added that point
+      bool_arr = map(y -> almost_equal(x, y, ϵ), y_arr[end]) # check if we already added that point
       if !in(true, bool_arr) # if not
-        push!(x_arr[end], x) # add it to the array
+        push!(y_arr[end], x) # add it to the array
         x = λ*x*(1-x) # iterate once more
       else
         break # else break from the while loop
       end
     end
+    push!(x_arr[end], λ)
   end
-  return x_arr
+  return x_arr, y_arr
 end
