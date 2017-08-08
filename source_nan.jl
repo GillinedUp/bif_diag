5b9b0caeb8570c8a03648a0e7c8e2bb8cdbf236b
GillinedUp <myrreah@gmail.com>
2017-08-08 14:46:46 +0300

    minor refactoring

--- source_nan.jl
+++ source_nan.jl
@@ -29,3 +29,34 @@ function calc_bif_nan(x_a, p)
   end
   return λ_arr, y_arr
 end
+
+
+x_a = 0.5
+p = 1000
+
+λ_arr = collect(linspace(0.0, 4.0, p))
+p_const = 4407 # calculated with find_eq()
+iter_const = 100 # make const
+ϵ = 1e-6
+y_arr = Matrix{Float64}(p, p_const)
+fill!(y_arr, NaN)
+for i = 1:p
+  λ = λ_arr[i]
+  x = x_a
+  for j = 1:iter_const
+    x = λ*x*(1-x)
+  end
+  k = 1
+  while(true)
+    bool_arr = map(y -> almost_equal(x, y, ϵ), y_arr[i, :])
+    if !in(true, bool_arr) # if not
+      while(!isequal(y_arr[i, k],NaN))
+        k += 1
+      end
+      y_arr[i, k] = x # add it to the array
+      x = λ*x*(1-x) # iterate once more
+    else
+      break # else break from the while loop
+    end
+  end
+end
--- tools.jl
+++ tools.jl
@@ -1,11 +1,11 @@
-function calc_max(x_a, precision)
-  λ_arr = collect(linspace(0.0, 4.0, precision))
+function calc_max(x_a, p)
+  λ_arr = collect(linspace(0.0, 4.0, p))
   iter_const = 100
   ϵ = 1e-6
-  x_arr = Array{Array{Float64, 1}, 1}(precision)
-  y_arr = Array{Array{Float64, 1}, 1}(precision)
+  x_arr = Array{Array{Float64, 1}, 1}(p)
+  y_arr = Array{Array{Float64, 1}, 1}(p)
   max = 0
-  for j = 1:precision
+  for j = 1:p
     λ = λ_arr[j]
     x = x_a
     x_arr[j] = Array{Float64, 1}(0)
